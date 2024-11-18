5e06c9bc66bd7956d2c80f179f3e95faa8d18763
Marcus <rinkajhin@gmail.com>
2024-11-18 08:34:15 +0800

    *practice*

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:fordago_application/screens/mainscreen.dart';
+
+void main() {
+  runApp(
+    myApp(),
+  );
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Mainscreen(),
+    );
+  }
+}
