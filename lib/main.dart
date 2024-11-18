c0f47cf9463d00f55070977aacb17bd6ffa8e3c5
Marcus Quibol <158299511+rinkaji@users.noreply.github.com>
2024-11-18 09:15:18 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Mainscreen(),
     );
   }
